06a8d78e6241be23859dc25779ad1ad15b23c9c7
Melissa Galan <152249508+QuokKae@users.noreply.github.com>
2024-02-18 14:38:28 -0800

    Updated files

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+console.log('text')
+
+// sets up canvas variables
+const canvas = document.getElementById("jumpgame");
+const ctx = canvas.getContext("2d");
+
+// sets game size
+const game_width = canvas.style.width = '70vw';
+const game_height = canvas.style.height = '40vh';
